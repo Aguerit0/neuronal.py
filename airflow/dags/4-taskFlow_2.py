72c17d10cb7ef66ced3f27bc5ee96d7d4aa44733
Esteban Agüero <cuentauniversidading@gmail.com>
2024-10-14 09:58:10 -0300

    taskFlow dag 2

--- airflow/dags/4-taskFlow_2.py
+++ airflow/dags/4-taskFlow_2.py
@@ -1,8 +1,17 @@
+"""
+DESCRIPCIÓN
+la idea es realizar una obtención del último precio (de alguna crypto),
+aplicarle un indicador al precio para ver si existe una posible C/V
+"""
+
+
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 
 import requests
 import pandas as pd
+#pip install pycoingecko matplotlib
+
 import pycoingecko
 
 
@@ -22,9 +31,16 @@ data =
 def etl_btc_data():
     @task
     def extract_btc_price():
-        return requests.get(API).json()['bitcoin']
-    
-    
+        data_crypto = coinGecko.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days=30)
+        dates = [data[0] for data in data_crypto['prices']]
+        dates = [
+            datetime.datetime.fromtimestamp(date/1000)
+            for date in dates
+        ]
+        prices = [data[1] for data in data_crypto['prices']]
+        return pd.DataFrame({'date': dates, 'price': prices})
+        
+        
     @task
     def process_data(data):
-        
+        df = pd.DataFrame(data)
